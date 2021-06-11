3e4d4a25337bb19d0478c212ec6291d37f9a5016
Are Edvardsen <biorakel@gmail.com>
2021-06-11 15:32:56 +0200

    clean-up

--- plugins/login-otp.php
+++ plugins/login-otp.php
@@ -3,9 +3,3 @@ include_once "plugins/login-otp.php";
     
 return new AdminerLoginOtp(base64_decode($_ENV['ADMINER_OTP_SECRET']));
 
-#$plugins = array(
-#  new AdminerLoginOtp(base64_decode($_ENV['ADMINER_OTP_SECRET'])),
-#);
-    
-#return new AdminerPlugin($plugins);
-
